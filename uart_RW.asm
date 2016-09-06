1655bdb4d01f896883753661a781aca84cf980e2
acidrain78 <staspi@yandex.ru>
2016-09-06 10:01:24 +0300

    Update uart_RW.asm

--- uart_RW.asm
+++ uart_RW.asm
@@ -1,83 +1,89 @@
-;UART_DAT EQU 0xF8EF ;DAT и DLL одинаковые, так надо
+	
+UART_DAT EQU 0xF8EF ;DAT и DLL одинаковые, так надо
 UART_DLL EQU 0xF8EF 
 UART_DLM EQU 0xF9EF 
 UART_FCR EQU 0xFAEF 
 UART_LCR EQU 0xFBEF 
 UART_LSR EQU 0xFDEF 
-RTC_REG  EQU 0xDEF7 
+RTC_REG EQU 0xDEF7 
 RTC_DATA EQU 0xBEF7 
-; ВНИМАНИЕ!!!
-; СКОРОСТЬ УАРТ поднял до 115200. делитель с 2х поменял на 1! По инструкции делитель
-; должен быть = 5!!! ДимкаМ - поясни!
-	
+
+
 ;инициализация
 UART_INIT: 
-	LD BC,UART_LCR	;переключаем DAT в режим DLL
-	LD A,%10000000
-	OUT (C),A
-	LD BC,UART_DLL	;настраивам делитель скорости(115200/n) в baseconf не выше 56700
-	LD A,1		;т.е. делить будем на два 115200/2=57600
-	OUT (C),A		;закидываем младший байт делителя
-	LD BC,UART_DLM
-	ld a,0			;и старший ноль соответственно
-	OUT (C),A
-	LD BC,UART_LCR	;переключаем DLL в режим регистра данных
-	LD A,3			;заодно 1 стопбит и без контроля чётности и без управления потоком
-	OUT (C),A
-UART_FIFO_RESET:
-	ld bc,UART_FCR	;сбрасываем буферы
-	ld a,7
-	out (c),a
-	ret
+
+	ld	bc,0xfbef       	;LCR
+	ld	a,0x83
+	out	(c),a
+	ld	b,0xf8          	;DLL
+	ld	de,1			;делитель 1 - 115200; 2 - 57600
+	out	(c),e
+	inc	b               	;DLM
+	out	(c),d
+	ld	b,0xfc          	;MCR
+	ld	a,0x03
+	out	(c),a
+	dec	b               	;LCR
+	ld	a,0x03
+	out	(c),a
+
+UART_FIFO_RESET
+
+	ld	bc,0xfaef       	;FCR
+	ld	a,0x07          	;FIFO reset
+	out	(c),a
+	ld	a,0x01
+	out	(c),A
+.clfifo:
+	ld	b,0xfd          	;LSR
+	in	a,(c)
+	and	0x01	
+	ret	z
+	ld	b,0xf8          	;DAT
+	in	a,(c)
+	jr	.clfifo
 	
 ;приём DE-байтов в по адресу HL. на выходе в A: ноль-ОК, неноль-ошибка
 ;лучше выключать прерывания на время приёмки. а то просрём дату, и будед переполнение фифо
-UART_READ:					;кагбы можно слать данные с ПЦ, будем ждать
-;		ld a,d
-;		or e
-;		ret z				;читать больше нинадо, выход с ОК, в А ноль
 
-		ld bc,UART_LSR		;читаем статус
+UART_READ:
+	dec	de
+	ld	a,d
+	or	e
+	ret	z			;читать больше нинадо, выход с ОК, в А ноль
+	ld	bc,UART_LSR		;читаем статус
+
 UART_READ_WAIT:
-;						;мое корявое дополнение для прерывания чтения из компорта тк
-						;может не остановиться и зациклиться
-		ld	a,e
-		or	d
-		ret z
-		in a,(c)		
-		bit 1,a			;проверяем на переполнение буфера
-;		ret nz			;выход с ошибкой, в А неноль
-;		dec	de
-		bit 0,a			;проверяем есть ли чего в буфере приёма
-		jr z,UART_READ_WAIT	;если буфер пустой то ждём
-		ld bc,#f8ef		;UART_DAT		;читаем байт
-		in a,(c)
-		cp	10		; тут acid пытался сделать проверку байта на конец пакета от ЕСП. Но не сработало
-		jr	nz,.skp1
-		ld	(hl),a
-		inc	hl
-		ret				;выход тк данных больше не будет
 
-.skp1		ld	(hl),a
-		inc hl
-		dec	de
-		jr UART_READ
+	in	a,(c)		
+	and	1
+	ret	z
+;	bit	1,a			;проверяем на переполнение буфера
+					;выход с ошибкой, в А неноль
+;	ret	nz
+;	bit	0,a			;проверяем естьли чего в буфере приёма
+;	jr	z,UART_READ		;если буфер пустой то ждём
+	ld	bc,UART_DAT		;читаем байт
+	in	a,(c)
+	ld	(hl),a
+	inc	hl
+	cp	10
+	ret	z
+
+	jr	UART_READ
 	
-;отправка DE-байтов начиная с адреса HL. на выходе в A: ноль-ОК, неноль-ошибка
-UART_WRITE:	;кагбы ПЦ уже должен быть готов принимать дату
+;отправка. на выходе в A: ноль-ОК, не нуль - ошибка
 
-
-		ld bc,UART_LSR	;читаем статус
-
-UART_WRITE_WAIT:
-		in a,(c)		
-		bit 6,a			;ждём отправки предыдущего байта
-		jr z,UART_WRITE_WAIT	;если буфер непустой то ждём
-		ld bc,#f8ef		;UART_DAT	;отсылаем байт
-		ld a,(hl)
-		inc hl
-		out (c),a
-		cp	10
-		ret	z
-
-		jr UART_WRITE
+UART_WRITE:
+      ld	bc,0xfdef       ;LSR
+_putch1:
+	in	a,(c)	
+	and	0x20
+	jr	z,_putch1
+	ld	b,0xf8		;DAT
+	ld	a,(hl)
+	out	(c),a
+	cp	13
+	ret	z
+	inc	hl
+	jr	UART_WRITE
